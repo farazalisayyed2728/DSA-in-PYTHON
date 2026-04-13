9333313e7e7ca7c636a0a81452e110d867487599
Sayyed Faraz Ali <farazalisayyed2728@gmail.com>
2026-04-13 19:24:08 +0530

    Add array3

--- ARRAY_in_PYTHON/array2.py
+++ ARRAY_in_PYTHON/array2.py
@@ -9,3 +9,6 @@ for i in range(0,n):
 
 for x in arr:
     print(x , end=" ")
+
+#this is from user 
+
---
+++ ARRAY_in_PYTHON/array3.py
@@ -0,0 +1,7 @@
+from array import *
+
+arr = array('i', [11,22,33,44,55,66,77,88,99] )
+
+i = arr.index(88)
+
+print(i)
