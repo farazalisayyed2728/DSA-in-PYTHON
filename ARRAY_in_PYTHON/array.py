777be6a71f23b0a4abadcd672c57d2eb6bbf1493
Sayyed Faraz Ali <farazalisayyed2728@gmail.com>
2026-04-13 19:08:11 +0530

    updated array

--- ARRAY_in_PYTHON/array.py
+++ ARRAY_in_PYTHON/array.py
@@ -1,11 +1,36 @@
-import array as arr
-val = arr.array('i',[1,2,3,4,5,6])
+from array import *
+val = array('i',[1,2,3,4,5,6])
 
-for i in range(0,6):
-    print(val[i], end=" ")
+# for i in range(0,len(val)):
+#     print(val[i], end=" ")
 
-print('\n')
+# print('\n')
 
 
-for x in val:
-    print(x, end=",")
+# for x in val:
+
+#     print(x, end=",")
+# print('\n')
+
+# print(val.typecode)
+
+# val.reverse()
+# for i in range(0,len(val)):
+#     print(val[i], end=" ")
+
+
+
+# val.insert(0,50)
+# val.append(100)
+# val[3] = 44
+
+copyArray = array(val.typecode, (x*2 for x in val))
+
+# copyArray.pop(3)
+# copyArray.remove(12)
+SA = val[0 : 3]
+
+
+for i in range(0,len(SA)):
+    print(SA[i], end=" ")
+
