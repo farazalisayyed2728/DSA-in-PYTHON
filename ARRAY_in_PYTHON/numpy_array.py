6a4cfa5bbc907c8f7581522da2e8d61743347026
Sayyed Faraz Ali <farazalisayyed2728@gmail.com>
2026-05-03 18:33:55 +0530

    add numpy_array.py

--- ARRAY_in_PYTHON/numpy_array.py
+++ ARRAY_in_PYTHON/numpy_array.py
@@ -1,5 +1,10 @@
 from numpy import *
-val = array([1,2,3,4])
+# val = array([1,2,3,4])
+
+# for x in val:
+#     print(x, end=" ")
+
+val = linspace(10,20 ,5)
 
 for x in val:
     print(x, end=" ")
