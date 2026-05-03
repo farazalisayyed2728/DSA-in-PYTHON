9a6de8d6f55bd6c2c4ee58c3fbcdac83d9a5df41
Sayyed Faraz Ali <farazalisayyed2728@gmail.com>
2026-05-03 18:49:10 +0530

    add multi_dimensional_array

---
+++ ARRAY_in_PYTHON/multi_dimensional_array.py
@@ -0,0 +1,16 @@
+from numpy import *
+zero = array(10)
+print(zero)
+
+one = array([1,2,3,4,5])
+print(one)
+
+# #two dimensional aray is a collection od one dimensional array
+
+two = array([ [1,2,3],[4,5,6],[7,8,9] ])
+print(two)
+
+#three dimensional array is a collection of twwo dimensional array
+
+three = array([  [[1,2],[3,4] ] , [[5,6],[7,8] ]  ])
+print(three)
